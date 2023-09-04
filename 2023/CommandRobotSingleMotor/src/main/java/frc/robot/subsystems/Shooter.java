92d2218254d11f0fa800e6878f459896aea42f64
Alex Levin <alevin@gmail.com>
2023-09-03 20:15:03 -0700

    mix

--- 2023/CommandRobotSingleMotor/src/main/java/frc/robot/subsystems/Shooter.java
+++ 2023/CommandRobotSingleMotor/src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,23 +7,24 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 // import com.ctre.phoenix.motorcontrol.can.TalonFX;
 // import harkerrobolib.wrappers.HSFalcon;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
-  WPI_TalonFX m1_Motor;
+  TalonFX m1_Motor;
 
   /** Creates a new shooter. */
   public Shooter() {
 
-    m1_Motor = new WPI_TalonFX(Constants.MOTOR1, "CAN_Network");
+    m1_Motor = new TalonFX(Constants.MOTOR1, "CAN_Network");
 
     /**
      * Configure the current limits that will be used Stator Current is the current that passes
@@ -33,8 +34,23 @@ public class Shooter extends SubsystemBase {
      *
      * <p>enabled | Limit(amp) | Trigger Threshold(amp) | Trigger Threshold Time(s)
      */
-    m1_Motor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 2, 3, 1.0));
-    m1_Motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 1, 2, 0.5));
+    // m1_Motor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 2, 3, 1.0));
+    //  m1_Motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 1, 2, 0.5));
+
+    CurrentLimitsConfigs m_currentLimits = new CurrentLimitsConfigs();
+
+    TalonFXConfiguration toConfigure = new TalonFXConfiguration();
+    m_currentLimits.SupplyCurrentLimit = 1; // Limit to 1 amps
+    m_currentLimits.SupplyCurrentThreshold = 4; // If we exceed 4 amps
+    m_currentLimits.SupplyTimeThreshold = 1.0; // For at least 1 second
+    m_currentLimits.SupplyCurrentLimitEnable = true; // And enable it
+
+    m_currentLimits.StatorCurrentLimit = 20; // Limit stator to 20 amps
+    m_currentLimits.StatorCurrentLimitEnable = true; // And enable it
+
+    toConfigure.CurrentLimits = m_currentLimits;
+
+    m1_Motor.getConfigurator().apply(toConfigure);
   }
 
   @Override
@@ -44,15 +60,19 @@ public class Shooter extends SubsystemBase {
 
   public void runShooter(XboxController controller) {
     // m1_Motor.set(ControlMode.PercentOutput, controller.getLeftY() * Constants.SPEED);
-    m1_Motor.set(ControlMode.PercentOutput, controller.getRightTriggerAxis() * Constants.SPEED);
+    DutyCycleOut m_output = new DutyCycleOut(0);
+    m1_Motor.setControl(m_output.withOutput(controller.getRightTriggerAxis() * Constants.SPEED));
     // System.out.println("getY = " + controller.getLeftY());
   }
 
   public void driveForward() {
-    m1_Motor.set(ControlMode.PercentOutput, 0.5 * Constants.SPEED);
+    DutyCycleOut m_output = new DutyCycleOut(0);
+    /// m1_Motor.set(ControlMode.PercentOutput, 0.5 * Constants.SPEED);
+    m1_Motor.setControl(m_output.withOutput(0.5 * Constants.SPEED));
   }
 
   public void stop() {
-    m1_Motor.set(ControlMode.PercentOutput, 0);
+    DutyCycleOut m_output = new DutyCycleOut(0);
+    m1_Motor.setControl(m_output.withOutput(0));
   }
 }
